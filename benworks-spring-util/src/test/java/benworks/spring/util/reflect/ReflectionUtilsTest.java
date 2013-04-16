805f93f72a48b115e650ab07f2c76fe3d0a026b2
aben328 <aben328@gmail.com>
2013-04-16 12:49:06 +0800

    xml extensible example

---
+++ benworks-spring-util/src/test/java/benworks/spring/util/reflect/ReflectionUtilsTest.java
@@ -0,0 +1,19 @@
+package benworks.spring.util.reflect;
+
+/**
+ * Understanding tools use ReflectionUtils.
+ * 
+ * @author Ben
+ *
+ */
+public class ReflectionUtilsTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
