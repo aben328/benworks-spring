4aec64f24ccbef22fcb9879dfbea4351e4da6875
aben328 <aben328@gmail.com>
2013-04-16 13:34:25 +0800

    pring -> spring

---
+++ benworks-spring-bean/src/main/java/benworks/spring/bean/factory/xml/people/PeopleNamespaceHandler.java
@@ -0,0 +1,13 @@
+package benworks.spring.bean.factory.xml.people;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+
+public class PeopleNamespaceHandler extends NamespaceHandlerSupport {
+
+	@Override
+	public void init() {
+		registerBeanDefinitionParser("people", new PeopleBeanDefinitionParser());
+	}
+
+}
